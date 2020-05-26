aa97b9b023dbba5821e7f83a57eb05bd6b15d406
Sergio 2 <sgrifell@gmail.com>
2020-05-26 13:26:31 +0200

    Programa interactivo

--- hola.php
+++ hola.php
@@ -1,4 +1,5 @@
 <?php
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+// Autor: Sergio Gómez <sergio@uco.es>
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
